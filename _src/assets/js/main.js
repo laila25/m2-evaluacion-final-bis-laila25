42671a7c6cd39d9db5d86c6a9226ccb1bdbf23c5
laila25 <laila2519@gmail.com>
2019-08-22 10:48:04 +0200

    add js function toggle hidden

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,8 +3,12 @@
 const button = document.querySelector(".js-btn");
 const select = document.querySelectorAll(".js-radio");
 const cardList = document.querySelector(".js-list");
+
+let cardListLi = [];
 let radioSelect = "";
 let cards = [];
+let frontImages = [];
+let backImages = [];
 
 function getRadioSelected() {
   //debugger;
@@ -40,11 +44,26 @@ function getDataFromServer() {
 function paintData() {
   cardList.innerHTML = "";
   for (let i = 0; i < cards.length; i++) {
-    cardList.innerHTML += ` <li class="main_content_list_item"></li>`;
-    cardList.innerHTML += ` <li class="main_content_list_item2"><img src="${
+    cardList.innerHTML += ` <li class="main_content_list_item js-list-li"><img src="${
       cards[i].image
-    }"></li>`;
+    }" class="front_img hidden" data-img="front">
+    <img src="https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB" class="back_img" data-img="back"></li>`;
   }
+
+  cardListLi = document.querySelectorAll(".js-list-li");
+
+  for (const li of cardListLi) {
+    li.addEventListener("click", changeVisibility);
+  }
+}
+
+function changeVisibility(ev) {
+  const liSelectedFront = ev.currentTarget.firstChild;
+  const liSelectedBack = ev.currentTarget.lastChild;
+  console.log(liSelectedFront);
+  console.log(liSelectedBack);
+  liSelectedFront.classList.toggle("hidden");
+  liSelectedBack.classList.toggle("hidden");
 }
 
 button.addEventListener("click", getAndPaintData);
